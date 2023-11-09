ba8e0728a98a6ba9633af713cbc1771033a24801
z1x1c <>
2023-11-10 11:09:01 +1300

    Refactor bionic_pdf function to accept a pdf_path parameter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import fitz
 import flet as ft
+from flet import CrossAxisAlignment, FilePicker, Column, ElevatedButton, MainAxisAlignment, Row, Text, FilePickerResultEvent, icons, alignment
+import os
+import subprocess
 
 def extract_text(pdf_path):
     doc = fitz.open(pdf_path)
@@ -66,19 +69,56 @@ def make_bionic(text, font_size):
 
     return new_pdf
 
-def bionic_pdf():
-    pdf_path = './test/sample-text-3.pdf'
+def bionic_pdf(pdf_path):
+    # pdf_path = './test/sample-text-3.pdf'
     extracted_text = extract_text(pdf_path)  # Use the new extract_text function
 
     # Create a new bionic PDF with the extracted text
     new_pdf = make_bionic(extracted_text, 11)
 
+    directory, original_filename = os.path.split(pdf_path)
+    basename, ext = os.path.splitext(original_filename)
+    new_filename = f"{basename}-bionic{ext}"
+    new_pdf_path = os.path.join(directory, new_filename)
+
     # Save the new PDF
-    new_pdf.save("./test/bionic-text.pdf")
+    new_pdf.save(new_pdf_path)
     new_pdf.close()
+
+    return new_pdf_path
 
 def main(page: ft.Page):
     page.title = "Bionic Text Converter"
+
+    info_text = Text()
+
+    def file_picked(e: FilePickerResultEvent):
+        if e.files:
+            uploaded_file = e.files[0]
+            new_pdf_path = bionic_pdf(uploaded_file.path)
+            subprocess.call(['open', new_pdf_path])
+        else:
+            info_text.value = "Please upload a PDF file."
+        page.update()
+
+    file_picker = FilePicker(on_result=file_picked)
+    
+    drop_zone = ElevatedButton(
+        text="Click here to select a PDF file.",
+        icon=icons.UPLOAD_FILE,
+        on_click=lambda _: file_picker.pick_files(),
+    )
+
+    col = Column(
+            controls=[drop_zone, file_picker, info_text],
+            alignment=MainAxisAlignment.CENTER,
+        )
+    row = Row(
+            controls=[col],
+            alignment=MainAxisAlignment.CENTER,
+            expand=True,
+            )
+    page.add(row)
 
 if __name__ == "__main__":
     ft.app(target=main)
